15c9ed35a23d0dab4e7adbe586170e3119fc7a3c
Jan Bouček <jan.boucek@woltair.cz>
2024-10-06 16:16:19 +0200

    fix depletion

--- pheromone_layer.gd
+++ pheromone_layer.gd
@@ -122,7 +122,8 @@ func draw_pheromone_at_position(
 		if additive:
 			var previous_value = grid_data[y][x]
 			var new_value = min(1.0, previous_value + interpolation_value * value)
-			added_total = new_value - previous_value
+			added_total += new_value - previous_value
+#			print("added_total", added_total)
 			grid_data[y][x] = new_value
 		else:
 			var new_value = interpolation_value * value
