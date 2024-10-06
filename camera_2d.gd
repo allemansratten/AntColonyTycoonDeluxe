d097a814707fec39c2f8d70ca2d780e1bd82f37f
Václav Volhejn <vaclav.volhejn@gmail.com>
2024-10-06 13:51:06 +0200

    Zoom multiplicatively and not additively

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-@export var zoom_speed: float = 0.01 # Adjust this to control zoom speed
+@export var zoom_speed: float = 0.002 # Adjust this to control zoom speed
 @export var min_zoom: float = 1.0 # Minimum zoom level
 @export var max_zoom: float = 4.0 # Maximum zoom level
 
@@ -9,9 +9,11 @@ func _ready():
 	zoom = Vector2(max_zoom, max_zoom)
 
 func _process(delta: float):
-	# Gradually increase the zoom value
-	zoom.x -= zoom_speed * delta
-	zoom.y -= zoom_speed * delta
+	# Gradually increase the zoom value.
+	# Multiplicative and not additive increase because otherwise the zoom speeds up.
+	# The time to zoom from 4 to 2 should be the same as from 2 to 1.
+	zoom.x /= (1 + zoom_speed) ** delta
+	zoom.y /= (1 + zoom_speed) ** delta
 	
 	# Clamp the zoom value between min and max
 	zoom.x = clamp(zoom.x, min_zoom, max_zoom)
